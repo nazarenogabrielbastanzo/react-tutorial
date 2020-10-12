54de0824bd5480489f8714beca4107eaaf0a0b39
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-10-12 09:32:31 -0300

    Añadido estilo de Bootstrap

--- src/Table.js
+++ src/Table.js
@@ -3,27 +3,32 @@ import React, {Component} from 'react'
 class Table extends Component {
     render() {
         return (
-            <table>
+            <table className="table">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Job</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Job</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
+                        <th scope="row">1</th>
                         <td>Charlie</td>
                         <td>Janitor</td>
                     </tr>
                     <tr>
+                        <th scope="row">2</th>
                         <td>Mac</td>
                         <td>Bouncer</td>
                     </tr>
                     <tr>
+                        <th scope="row">3</th>
                         <td>Dee</td>
                         <td>Aspiring actress</td>
                     </tr>
                     <tr>
+                        <th scope="row">4</th>
                         <td>Dennis</td>
                         <td>Bartender</td>
                     </tr>
