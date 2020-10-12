24045d77d3950ad6ad74eb17eb130296dd25603d
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-10-12 10:16:48 -0300

    Componentes simples

--- src/Table.js
+++ src/Table.js
@@ -1,38 +1,50 @@
 import React, {Component} from 'react'
 
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Charlie</td>
+                <td>Janitor</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Dee</td>
+                <td>Aspiring actress</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    )
+}
+
 class Table extends Component {
     render() {
         return (
             <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         )
     }
