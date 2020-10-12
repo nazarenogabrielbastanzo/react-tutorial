242f4c5cb087b1d967ed8681dcb77ad4f6e0b440
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-10-12 09:44:51 -0300

    Nuevo componente de clase personalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Header from './Header'
 import Table from './Table'
 
 class App extends Component {
     render() {
         return(
             <div className="container">
+                <Header />
                 <Table />
             </div>
         )
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <h1 className="display-4 text-center">Hola Mundo</h1>
+        )
+    }
+}
+
+export default Header
