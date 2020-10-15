54c6910290d2952dd6e8b293133d766e89bb275b
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-10-15 15:01:15 -0300

    Updates

---
+++ precache-manifest.f2c248c96135cd34bab793b143281e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038f8595eeb4ba6026f3a7060d83b0db",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "16e07f94ec75723683f4",
+    "url": "/react-tutorial/static/css/main.c1122256.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e07f94ec75723683f4",
+    "url": "/react-tutorial/static/js/main.49e5af08.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
